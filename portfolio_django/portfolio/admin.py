0212c9b85ebac525542067aa8c357d2a66237f03
Yuji <g.yujinak@gmail.com>
2024-05-14 17:04:31 -0300

    exercício django admin

--- portfolio_django/portfolio/admin.py
+++ portfolio_django/portfolio/admin.py
@@ -2,3 +2,11 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Post
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'author', 'status')
+    search_fields = ['title', 'author', 'content']
+    prepopulated_fields = {'slug':('title',)}
+
+
+admin.site.register(Post, PostAdmin)
--- portfolio_django/portfolio/views/post_view.py
+++ portfolio_django/portfolio/views/post_view.py
@@ -5,4 +5,4 @@ from django.views import generic
 
 class PostView(generic.View):
     def get(self, request, *args, **kwargs):
-        return HttpResponse('Hello World')
+        return HttpResponse("<h1 style='color:blue'>Olá, Mundo!</h1>")
